0c0153964944b80c3c32e2c9ae36d52b8f9f4bfe
Ahlumin <magus52941@gmail.com>
2019-03-07 11:51:26 +0800

    Remove old codebase

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classnames from "classnames/bind";
+import style from "./style.scss";
+const cx = classnames.bind(style);
+
+export default class Home extends React.Component {
+  render() {
+    return <div className={cx("main")}>From App Yo.!</div>;
+  }
+}
