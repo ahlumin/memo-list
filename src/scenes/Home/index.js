48948b70bc9abb93115efe492d049ebfd6933769
Ahlumin <magus52941@gmail.com>
2019-03-07 17:14:46 +0800

    Add checkAuth mechanism in Home

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,12 +2,32 @@ import React from "react";
 import classnames from "classnames/bind";
 import style from "./style.scss";
 import SignIn from "./components/SignIn";
+import { Authentication } from "services";
+
 const cx = classnames.bind(style);
 
 export default class Home extends React.Component {
+  state = {
+    isInitializing: true,
+    hasAuth: false
+  };
+
+  componentDidMount() {
+    Authentication.checkAuth().then(hasAuth => {
+      this.setState({
+        hasAuth,
+        isInitializing: false
+      });
+    });
+  }
+
   render() {
-    return 1 === 1 ? (
-      <div className={cx("main")}>From App Yo.!</div>
+    const { isInitializing, hasAuth } = this.state;
+
+    return isInitializing ? (
+      <div className={cx("main")}>LOADING...</div>
+    ) : hasAuth ? (
+      <div className={cx("main")}>From App Yo!</div>
     ) : (
       <SignIn />
     );
