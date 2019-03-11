b3b56c00a667d41ccc8f6c13272457a26ccf1827
Ahlumin <magus52941@gmail.com>
2019-03-11 14:49:14 +0800

    Refactor authetication service

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -2,19 +2,28 @@ import Firebase from "./firebase.js";
 require("firebase/auth");
 const provider = new Firebase.auth.GoogleAuthProvider();
 
-export default {
-  signIn: function() {
-    return Firebase.auth().signInWithPopup(provider);
-  },
-  checkAuth: function() {
-    return new Promise(resolve => {
-      Firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+function signIn() {
+  return Firebase.auth().signInWithPopup(provider);
+}
+
+function checkAuth() {
+  return new Promise(resolve => {
+    Firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
-  }
+  });
+}
+
+function getUserInfo() {
+  return Firebase.auth().currentUser;
+}
+
+export default {
+  signIn,
+  checkAuth,
+  getUserInfo
 };
