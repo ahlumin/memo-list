64c7b048d64de7efefab419b521aa7202c284a18
Ahlumin <magus52941@gmail.com>
2019-03-15 15:27:30 +0800

    Add Loading component

---
+++ src/components/Record/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames/bind";
+import style from "./style.scss";
+const cx = classnames.bind(style);
+
+function Record({ record }) {
+  const { day, consume, isCredit } = record;
+
+  return (
+    <div className={cx("record")}>
+      <span className={cx("day")}>{day}</span>
+      <span className={cx("consume")}>{consume}</span>
+      <span className={cx("credit")}>{isCredit ? "YES" : "NO"}</span>
+    </div>
+  );
+}
+
+Record.propTypes = {
+  record: PropTypes.object.isRequired
+};
+
+export default Record;
