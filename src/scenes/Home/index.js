a4ad9a3b0b1da687f09cce62546324aca6526089
Ahlumin <magus52941@gmail.com>
2019-03-15 10:47:08 +0800

    Refactor Home component in function component

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,31 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames/bind";
 import style from "./style.scss";
-const TODAY = new Date();
 
 const cx = classnames.bind(style);
 
-export default class Home extends React.Component {
-  static propTypes = {
-    uid: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired
-  };
-
-  state = {
-    year: TODAY.getFullYear(),
-    month: TODAY.getMonth + 1,
-    records: []
-  };
-
-  render() {
-    const { email, uid } = this.props;
-
-    return (
-      <div className={cx("main")}>
-        From App Yo!
-        <p>uid: {uid}</p>
-        <p>email: {email}</p>
-      </div>
-    );
-  }
+function Home({ uid, email }) {
+  return (
+    <div className={cx("main")}>
+      From App Yo!
+      <p>uid: {uid}</p>
+      <p>email: {email}</p>
+    </div>
+  );
 }
+
+Home.propTypes = {
+  uid: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
+};
+
+export default Home;
