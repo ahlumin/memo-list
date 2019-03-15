572b481f6f64ae1d519a01e1face838ddaef1aab
Ahlumin <magus52941@gmail.com>
2019-03-15 15:43:04 +0800

    Modify for re-export setting

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Loading } from "./Loading";
+export { default as Record } from "./Record";
+export { default as Field } from "./Field";
