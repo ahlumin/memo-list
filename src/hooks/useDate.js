0dab8e661a1ac39b2741e9bb08f1f987421876ba
Ahlumin <magus52941@gmail.com>
2019-03-15 13:05:19 +0800

    Modify useDate schema

--- src/hooks/useDate.js
+++ src/hooks/useDate.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 export default function useDate() {
   const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth() + 1;
+  const year = currentYear;
+  const month = currentMonth;
 
   const [dateInfo, setDateInfo] = useState({
-    today,
+    currentYear,
+    currentMonth,
     year,
     month
   });
