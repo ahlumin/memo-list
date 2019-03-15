0e89805b42685889127fd7517100967eaab26d7b
Ahlumin <magus52941@gmail.com>
2019-03-15 11:39:06 +0800

    Modify App state for add isCloseLogInPop property

--- src/hooks/useAppState.js
+++ src/hooks/useAppState.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
 export default function useAppState() {
-  const [app, setApp] = useState({ isLoading: true });
+  const [app, setApp] = useState({
+    isLoading: true,
+    isCloseLogInPop: false
+  });
   return [app, setApp];
 }
